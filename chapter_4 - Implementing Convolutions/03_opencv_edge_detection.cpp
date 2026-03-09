41cb55c378ba288e0dbdd8589eee586effa7046f
Logan Mo <itsluqman55@gmail.com>
2026-03-09 11:32:43 +0500

    updated the edge detection code to actually do edge detection using vertical and horizontal edge detetion kernels

--- chapter_4 - Implementing Convolutions/03_opencv_edge_detection.cpp
+++ chapter_4 - Implementing Convolutions/03_opencv_edge_detection.cpp
@@ -1,19 +1,100 @@
-#include <opencv2/opencv.hpp>
 #include <iostream>
+#include <Eigen/Dense>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/eigen.hpp>
+
+auto Padded_Conv2D = [](const Eigen::MatrixXd& X, const Eigen::MatrixXd& K, int padding) {
+	const int X_rows = X.rows();
+	const int X_cols = X.cols();
+	const int K_rows = K.rows();
+	const int K_cols = K.cols();
+
+	if (X_rows < K_rows) throw std::invalid_argument("The input has less rows than the kernel");
+	if (X_cols < K_cols) throw std::invalid_argument("The input has less columns than the kernel");
+
+	const int rows = X_rows - K_rows + 2 * padding + 1;
+	const int cols = X_cols - K_cols + 2 * padding + 1;
+
+	auto fitting_dims = [&padding](int pos, int k, int length) {
+		int input = pos - padding;
+		int kernel = 0;
+		int size = k;
+		if (input < 0)
+		{
+			kernel = -input;
+			size += input;
+			input = 0;
+		}
+		if (input + size > length)
+		{
+			size = length - input;
+		}
+		return std::make_tuple(input, kernel, size);
+		};
+
+	Eigen::MatrixXd res = Eigen::MatrixXd::Zero(rows, cols);
+
+	for (int i = 0; i < rows; ++i)
+	{
+		const auto [input_i, kernel_i, size_i] = fitting_dims(i, K_rows, X_rows);
+		for (int j = 0; size_i > 0 && j < cols; ++j)
+		{
+			const auto [input_j, kernel_j, size_j] = fitting_dims(j, K_cols, X_cols);
+			if (size_j > 0)
+			{
+				auto input_roi = X.block(input_i, input_j, size_i, size_j);
+				auto in_kernel = K.block(kernel_i, kernel_j, size_i, size_j);
+				res(i, j) = input_roi.cwiseProduct(in_kernel).sum();
+			}
+		}
+	}
+	return res;
+	};
 
 int main() {
     
-    cv::Mat source = cv::imread("C:\\Users\\luqma\\Downloads\\flower.jpeg", cv::IMREAD_COLOR);
+	Eigen::MatrixXd G_x(3, 3), G_y(3, 3);
 
-    cv::Mat rotated, gray;
-    cv::rotate(source, rotated, cv::ROTATE_90_CLOCKWISE);
-    cv::cvtColor(rotated, gray, cv::COLOR_BGR2GRAY);
+	G_x <<
+		1., 0., -1.,
+		2., 0., -2.,
+		1., 0., -1.;
 
-    cv::imshow("source", source);
-    cv::imshow("rotated", rotated);
-    cv::imshow("gray", gray);
+	G_y <<
+		1., 2., 1.,
+		0., 0., 0.,
+		-1., -2., -1.;
 
-    cv::imwrite("leds_rot.jpg", gray);
+	cv::Mat src = cv::imread("C:\\Users\\luqma\\Downloads\\flower.jpeg", cv::IMREAD_COLOR);
+	cv::Mat img;
+
+	cv::cvtColor(src, img, cv::COLOR_BGR2GRAY);
+	Eigen::MatrixXd X;
+	cv::cv2eigen(img, X);
+
+	const int padding = (G_x.rows() - 1) / 2;
+	auto G_x_conv = Padded_Conv2D(X, G_x, padding);
+	auto G_y_conv = Padded_Conv2D(X, G_y, padding);
+
+	cv::Mat temp, G_x_res, G_y_res, res;
+
+
+
+	cv::eigen2cv(G_x_conv, temp);
+	temp.convertTo(G_x_res, CV_8UC1);
+	
+	cv::eigen2cv(G_y_conv, temp);
+	temp.convertTo(G_y_res, CV_8UC1);
+	
+	Eigen::MatrixXd G_res = G_x_conv + G_y_conv;
+	cv::eigen2cv(G_res, temp);
+	temp.convertTo(res, CV_8UC1);
+
+	cv::imshow("source", src);
+	cv::imshow("gray scale", img);
+	cv::imshow("applying Gx", G_x_res);
+	cv::imshow("applying Gy", G_y_res);
+	cv::imshow("applying both", res);
 
     cv::waitKey();
     
